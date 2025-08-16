d989a340e46a6a95ed1036155956f89d5e2f4fd8
cuongdinh98 <cuong.dinhduynd@gmail.com>
2025-08-16 20:21:13 +0200

    Bump version to 1.0.1

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,16 +1,3 @@
 fn main() {
-    // Add Windows-specific build configurations
-    #[cfg(target_os = "windows")]
-    {
-        // Set application manifest for Windows
-        if std::path::Path::new("Pluto-ToDo.exe.manifest").exists() {
-            println!("cargo:rustc-link-arg-bin=pluto-todo=/MANIFEST:EMBED");
-            println!("cargo:rustc-link-arg-bin=pluto-todo=/MANIFESTINPUT:Pluto-ToDo.exe.manifest");
-        }
-        
-        // Set Windows subsystem to avoid console window
-        println!("cargo:rustc-link-arg-bin=pluto-todo=/SUBSYSTEM:WINDOWS");
-    }
-    
     tauri_build::build()
 }
